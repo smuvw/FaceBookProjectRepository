d3d6b2bd8248134d8a76e0c8ead60062214cc5f5
smuvva <srinivasarao_muvva@yahoo.com>
2016-10-05 18:29:21 -0500

    moving files to Github

---
+++ FaceBook_Project/src/OpenApp.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class OpenApp {
+
+	public static void main(String[] args) {
+
+		FirefoxDriver driver =new FirefoxDriver();
+		
+		driver.get("http://facebook.com/");
+		
+		System.out.println("Facebook page is opened");
+
+	}
+
+}
